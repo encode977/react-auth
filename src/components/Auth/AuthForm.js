6a5a61cbe99d66fe719fede38e4d46241b7c6e41
Lokindra Dangi <pachar.com@gmail.com>
2021-10-02 01:13:17 +0545

    adding signin

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -21,38 +21,49 @@ const AuthForm = () => {
 
     //Validation to the input data
     setIsLoading(true);
+    let url;
     if (isLogin) {
+      url =
+        "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCKX8FPEqxIok_XdAti_movhflaPEiYJc0";
     } else {
-      fetch(
-        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCKX8FPEqxIok_XdAti_movhflaPEiYJc0",
-        {
-          method: "POST",
-          body: JSON.stringify({
-            email: enteredEmail,
-            password: enteredPassword,
-            returnSecureToken: true,
-          }),
+      url =
+        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCKX8FPEqxIok_XdAti_movhflaPEiYJc0";
+    }
 
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      ).then((res) => {
+    fetch(url, {
+      method: "POST",
+      body: JSON.stringify({
+        email: enteredEmail,
+        password: enteredPassword,
+        returnSecureToken: true,
+      }),
+
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => {
         setIsLoading(false);
         if (res.ok) {
+          return res.json();
         } else {
           return res.json().then((data) => {
             let errorMessage = "Auth failed";
             // if (data && data.error && data.error.message) {
             //   errorMessage = data.error.message;
             // }
-            alert(errorMessage);
+
+            throw new Error(errorMessage);
           });
         }
+      })
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => {
+        alert(err.message);
       });
-    }
   };
-
   return (
     <section className={classes.auth}>
       <h1>{isLogin ? "Login" : "Sign Up"}</h1>
